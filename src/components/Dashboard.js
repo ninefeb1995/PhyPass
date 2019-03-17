593583e42db701f5dc87b26eb9941eb79855e03d
Thinh Le <thinhle@kms-technology.com>
2019-03-17 15:49:45 +0700

    [add] bind data to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import { Conveyor } from './conveyor/Conveyor';
+import * as DashBoardService from '../app/services/dashboard';
 
 export class Dashboard extends Component {
     displayName = Dashboard.name;
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            listConveyors: []
+        };
+    }
+
+    componentDidMount() {
+        DashBoardService.getListOfConveyor(1, 50, (data) => {
+            if (data.data.err === 0) {
+                this.setState({listConveyors: data.data.data})
+            }
+        });
+    }
+    renderConveyor = () => {
+        let conveyors = [];
+        for (let i = 0; i < this.state.listConveyors.length; i++) {
+            let item = 
+                <div className="col-lg-4">
+                    <Conveyor information = {this.state.listConveyors[i]} />;
+                </div>
+            conveyors.push(item);
+        }
+        return conveyors;
+    }
+
     render() {
         return (
             <div className="row">
-                <div className="col-lg-4">
-                    <Conveyor />
-                </div>
-
-                <div className="col-lg-4">
-                    <Conveyor />
-                </div>
-
-                <div className="col-lg-4">
-                    <Conveyor />
-                </div>
+                {this.renderConveyor()}
             </div>
         );
     }
--- src/components/conveyor/Conveyor.js
+++ src/components/conveyor/Conveyor.js
@@ -4,23 +4,29 @@ import { Link } from 'react-router-dom';
 export class Conveyor extends Component {
     displayName = Conveyor.name;
     
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        const { information } = this.props;
+        console.log(information);
         return (
             <div className="card">
                 <div className="card-body bg-danger-800">
                     <div className="d-flex jc-center">
                         <Link to={'/conveyor/details'} className="btn rounded-round btn-xl bg-white">
-                            100%
+                            {information.stats * 100} %
                         </Link>
                     </div>
                     <div className="card-content">
                         <div className="text-center">
                             <Link to={'/conveyor/details'} className="font-xx-large">
-                                Conveyor 1
+                                Conveyor {information.id}
                             </Link>
                             <br />
                             <Link to={'/staff/details'} className="font-small">
-                                Nguyễn Viết Hùng
+                                {information.staff.name}
                             </Link>
                         </div>
                     </div>
