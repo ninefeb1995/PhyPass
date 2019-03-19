f140b44441389dcb06ce21fbe654e2f4d426bc6d
Thinh Le <thinhle@kms-technology.com>
2019-03-19 21:48:53 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import "./style.css";
+import "./styles/style.css";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
